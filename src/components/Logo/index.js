2c33d08e8d5fd5302215827780c0f91d237f00e5
pabasa <pabasa33@gmail.com>
2020-07-02 09:59:29 +0000

    Add files via upload

---
+++ src/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css';
+
+/**
+ * @author
+ * @function Hero
+ **/
+
+const Logo = (props) => {
+    return(
+        <div className="logo">
+            <a href="#"> PROSPER ABASA</a>
+        </div>
+    )
+
+}
+
+export default Logo
