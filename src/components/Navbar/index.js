4d482ca38921716f5dae812f7f1d987f339ff94f
pabasa <pabasa33@gmail.com>
2020-07-02 10:00:32 +0000

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import './style.css';
+import {NavLink} from "react-router-dom";
+
+
+/**
+ * @author
+ * @function Navbar
+ **/
+
+const Navbar = (props) => {
+
+    const [search, setSearch] = useState(false);
+
+    const openSearch = () => {
+        setSearch(true);
+    }
+
+    const searchClass = search ?'searchInput active':'searchInput';
+
+    const submitSearch = (e) => {
+        e.preventDefault();
+        //alert('working');
+    }
+
+    return(
+        <div className="navbar">
+           <ul className="navbarMenu">
+               <li><NavLink to="/">Home</NavLink> </li>
+               <li><NavLink to="/about-us">About Us</NavLink> </li>
+               <li><NavLink to="/post">Posts</NavLink> </li>
+               <li><NavLink to="/contact-us">Contact Us</NavLink> </li>
+           </ul>
+            <div className="search">
+                <form onSubmit={submitSearch}>
+                    <input type="text" placeholder="Search" className={searchClass}/>
+                    <img onClick={openSearch} className="searchIcon" src={require('../../assets/icons/search_icon.png')} alt="Search"/>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Navbar
